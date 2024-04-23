9e447924d4683cb0187713aec6b0b580c1a4a0b6
thwlckd <thwlckd@naver.com>
2024-04-23 16:21:50 +0900

    Refactor: 로컬스토리지 CRUD 작업 persist 함수 사용하도록 수정 - 로컬스토리지 유틸 파일 제거

--- src/store/useTodoStore.ts
+++ src/store/useTodoStore.ts
@@ -1,68 +1,53 @@
 import { create } from 'zustand';
 import { Todo } from '../types/todo';
-import { getLocalStorage } from '../utils/localStorage';
+import { persist } from 'zustand/middleware';
 
 interface State {
   todoList: Todo[];
 }
 
 interface Action {
-  actions: {
-    addTodo: (content: Todo['content']) => void;
-    toggleTodo: (id: Todo['id']) => void;
-    deleteTodo: (id: Todo['id']) => void;
-  };
+  addTodo: (content: Todo['content']) => void;
+  toggleTodo: (id: Todo['id']) => void;
+  deleteTodo: (id: Todo['id']) => void;
 }
 
-const useTodoStore = create<State & Action>((set) => ({
-  todoList: getLocalStorage(),
-  actions: {
-    addTodo: (content) =>
-      set(({ todoList }) => {
-        const id = crypto.randomUUID();
-        const newTodo = {
-          id,
-          content,
-          isDone: false,
-        };
-
-        localStorage.setItem(id, JSON.stringify(newTodo));
-
-        return {
-          todoList: [...todoList, newTodo],
-        };
-      }),
-    toggleTodo: (id) =>
-      set(({ todoList }) => ({
-        todoList: todoList.map((todo) => {
-          if (todo.id === id) {
-            const item = localStorage.getItem(id);
-
-            if (item) {
-              const { id, content, isDone } = JSON.parse(item);
-
-              localStorage.setItem(
-                id,
-                JSON.stringify({
-                  id,
-                  content,
-                  isDone: !isDone,
-                }),
-              );
-            }
-
-            return { ...todo, isDone: !todo.isDone };
-          } else return todo;
-        }),
-      })),
-    deleteTodo: (id) =>
-      set(({ todoList }) => {
-        localStorage.removeItem(id);
-
-        return { todoList: todoList.filter((todo) => todo.id !== id) };
-      }),
-  },
-}));
+const useTodoStore = create(
+  persist<State & Action>(
+    (set) => ({
+      todoList: [],
+      addTodo: (content) =>
+        set(({ todoList }) => ({
+          todoList: [
+            ...todoList,
+            {
+              id: crypto.randomUUID(),
+              content,
+              isDone: false,
+            },
+          ],
+        })),
+      toggleTodo: (id) =>
+        set(({ todoList }) => ({
+          todoList: todoList.map((todo) =>
+            todo.id === id ? { ...todo, isDone: !todo.isDone } : todo,
+          ),
+        })),
+      deleteTodo: (id) =>
+        set(({ todoList }) => ({
+          todoList: todoList.filter((todo) => todo.id !== id),
+        })),
+    }),
+    {
+      name: 'zustand-todo-list',
+    },
+  ),
+);
 
 export const useTodoList = () => useTodoStore((state) => state.todoList);
-export const useTodoActions = () => useTodoStore((state) => state.actions);
+export const useTodoActions = () =>
+  useTodoStore((state) => ({
+    addTodo: state.addTodo,
+    deleteTodo: state.deleteTodo,
+    toggleTodo: state.toggleTodo,
+  }));
